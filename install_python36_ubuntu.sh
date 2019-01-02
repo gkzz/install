71b49dc2757a2b30d77075be1649f1acb50e7de4
gkzz <gakuji.tamaki@gmail.com>
2019-01-02 13:54:11 +0900

    add python3.6 to packages lineup

--- install_python36_ubuntu.sh
+++ install_python36_ubuntu.sh
@@ -2,8 +2,8 @@
 
 ########################################################################################
 # Sources;
-# Get Docker CE for Ubuntu | Docker Documentation
-# https://docs.docker.com/install/linux/docker-ce/ubuntu/#upgrade-docker-ce
+# WSLの初期化からSSH設定、Gitのインストール、Githubとの連携、ターミナルの色の変更など
+# https://note.mu/gkz74/n/n1b716db7c8a9
 ########################################################################################
 
 startTime=$(date '+%d/%m/%Y %H:%M:%S');
@@ -21,6 +21,7 @@ sudo apt-get update -y
 ###### 1-3. saved_stdout_startTime()
 ###### 1-4. saved_stdout_endTime()
 ###### 1-5. main() 
+###### 1-6. install_python36_venv()
 ### 2. Execute My functions
 ########################################################################################
 
@@ -52,34 +53,35 @@ saved_stdout_startTime(){
 
 ###### 1-4. saved_stdout_endTime()
 saved_stdout_endTime(){
-    docker --version
+    python3.6 --version
     echo -e "$1\n$2\n$3" | tee $workingDirectory/log/stdout.log 
 }
 
-###### 1-5 main()
-######### Install Docker
+###### 1-5. main()
+######### Install python3.6
 main(){
-    sudo apt-get purge docker-ce -y
-    sudo chmod 777 /var/lib/docker/overlay2
-    sudo rm -rf /var/lib/docker
-    sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo apt-key fingerprint 0EBFCD88 -y
-    # pub   4096R/0EBFCD88 2017-02-22
-    #       Key fingerprint = 9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88
-    # uid                  Docker Release (CE deb) <docker@docker.com>
-    # sub   4096R/F273FCD8 2017-02-22
-    sudo add-apt-repository -y \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-    sudo apt-get install docker-ce -y
+    cd ~
+    cd /usr/bin
+    sudo apt install python3-pip python3-dev -y
+    sudo apt install python-pip python-dev -y
+    sudo add-apt-repository ppa:jonathonf/python-3.6 -y
+    sudo apt update -y
+    sudo apt install python3.6 python3.6-dev -y
+    sudo wget https://bootstrap.pypa.io/get-pip.py -y
+    sudo python3.6 get-pip.py 
 }
 
+###### 1-6. install_python36_venv()
+######### Install python3.6-venv
+install_python36_venv(){
+    # Soruces;
+    # python - How to use virtualenv with python3.6 on ubuntu 16.04? - Stack Overflow
+    # https://stackoverflow.com/questions/47822740/how-to-use-virtualenv-with-python3-6-on-ubuntu-16-04
+    sudo apt-get install python3-venv -y
+    sudo apt-get install python-virtualenv -y
+    virtualenv --python=python3.6 myvenv
+    sudo apt install python3.6-venv -y
+}
 
 ########################################################################################
 ### 2. Execute My functions 
@@ -95,17 +97,41 @@ else
     type_tags_only
 fi
 
-if ! docker --version >/dev/null 2>&1; then
+if ! python3.6 --version >/dev/null 2>&1; then
     main
-    endTime=$(date '+%d/%m/%Y %H:%M:%S'); 
-    runTime=$((end_time - start_time));
-    type_tags_only 
-    saved_stdout_endTime "startTime: $startTime" "endTime: $endTime" "You have finished installing docker in $runTime seconds!"
-    type_tags_only
 else
     endTime=$(date '+%d/%m/%Y %H:%M:%S');
     type_tags_only 
-    saved_stdout_endTime "startTime: $startTime" " endTime: $endTime" "You already have instaled docker."
+    saved_stdout_endTime "startTime: $startTime" " endTime: $endTime" "You already have instaled python3.6."
 fi
 
+cd ~
+mkdir python36-test
+cd python36-test
 
+if ! python3.6 -m venv 36 >/dev/null 2>&1; then
+    cd ~
+    cd ~/usr/bin
+    install_python36_venv
+    cd ~
+    cd python36-test
+fi
+
+if    python3.6 -m venv 36 ; then
+    source 36/bin/activate
+    deactivate
+    cd ~
+    rm -r python36-test
+    endTime=$(date '+%d/%m/%Y %H:%M:%S'); 
+    runTime=$((end_time - start_time));
+    type_tags_only 
+    saved_stdout_endTime "startTime: $startTime" "endTime: $endTime" "You have finished installing python3.6 in $runTime seconds!"
+    type_tags_only
+else
+    cd ~
+    rm -r python36-test
+    endTime=$(date '+%d/%m/%Y %H:%M:%S'); 
+    runTime=$((end_time - start_time));
+    type_tags_only 
+    saved_stdout_endTime "startTime: $startTime" "endTime: $endTime" "You couldn't install python3.6..."
+fi
